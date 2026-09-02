2cf18978160a442be595e896994f8672700d2455
tylerforgione <tyler.forgione26@gmail.com>
2026-09-02 14:40:55 -0400

    compute fractal noise

--- src/worldgen/terrain/noise.py
+++ src/worldgen/terrain/noise.py
@@ -1,18 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def generate_white_noise(width, height, seed):
+def generate_white_noise(width: int, height: int, seed: int):
     rng = np.random.default_rng(seed)
     return rng.random(size=(height, width), dtype=np.float32)
 
 # imprecise lerp
-def lerp(v0, v1, t):
+def lerp(v0: int, v1: int, t: float):
     return v0 + t * (v1 - v0)
 
-def smoothstep(t):
+# cubic Hermite
+def smoothstep(t: float):
     return (3 * t**2) - (2 * t**3)
 
-def generate_value_noise(width, height, seed, wavelength):
+def generate_value_noise(width: int, height: int, seed: int, wavelength: int):
     # find grid height and width
     grid_width = int(np.ceil(width / wavelength)) + 1
     grid_height = int(np.ceil(height / wavelength)) + 1
@@ -29,10 +30,10 @@ def generate_value_noise(width, height, seed, wavelength):
             cell_x = X // wavelength
             cell_y = Y // wavelength
 
-            top_left = grid[cell_y][cell_x]
-            top_right = grid[cell_y][cell_x + 1]
-            bottom_left = grid[cell_y + 1][cell_x]
-            bottom_right = grid[cell_y + 1][cell_x + 1]
+            top_left = grid[cell_y, cell_x]
+            top_right = grid[cell_y, cell_x + 1]
+            bottom_left = grid[cell_y + 1, cell_x]
+            bottom_right = grid[cell_y + 1, cell_x + 1]
 
             tx = (X % wavelength) / wavelength
             ty = (Y % wavelength) / wavelength
@@ -50,7 +51,27 @@ def generate_value_noise(width, height, seed, wavelength):
             # store interpolated value
             output[Y][X] = value
 
-    plt.imshow(X=output, cmap='gray')
-    plt.show()
+    return output
 
-generate_value_noise(512, 512, 12, 32)
+def generate_fractal_noise(width: int, height: int, seed: int, wavelength:int, octaves: int):
+    output = np.zeros(shape=(height, width), dtype=np.float32)
+    total_amplitude = 0.0
+
+    for i in range(octaves):
+        current_wavelength = int(wavelength / (2 ** i))
+
+        if current_wavelength < 1:
+            break
+        
+        amplitude = (1 / 2**i)
+        total_amplitude += amplitude
+
+        noise = generate_value_noise(width=width, height=height, seed=seed, wavelength=current_wavelength)
+        output += noise * amplitude
+
+    output /= total_amplitude
+    return output
+
+noise = generate_fractal_noise(1024, 1024, 56, 64, 4)
+plt.imshow(X=noise, cmap='gray')
+plt.show()
