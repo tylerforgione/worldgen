55bc8808005cc83cf676a81d47f94fb639ed5eb8
tylerforgione <tyler.forgione26@gmail.com>
2026-09-04 17:40:45 -0400

    use falloff continental masks

--- scripts/generate_world.py
+++ scripts/generate_world.py
@@ -1,16 +1,28 @@
 from worldgen.compute import initialize_compute
-from worldgen.terrain.heightmap import generate_heightmap
+from worldgen.terrain.heightmap import (
+    generate_continental_mask,
+    generate_heightmap,
+    generate_land_mask,
+)
 from worldgen.visualization.plot import plot_heightmap_3d
 
 initialize_compute()
 
 heightmap = generate_heightmap(
-    width=1024,
-    height=1024,
-    seed=8974,
+    width=2048,
+    height=2048,
+    seed=3982,
     wavelength=256,
-    octaves=8,
+    octaves=4,
     redistribution=4.0,
 )
 
-plot_heightmap_3d(heightmap)
+land_mask = generate_land_mask(heightmap=heightmap, sea_level=0.3)
+
+continental_mask = generate_continental_mask(
+    width=heightmap.shape[1], height=heightmap.shape[0]
+)
+
+plot_heightmap_3d(heightmap=continental_mask)
+plot_heightmap_3d(heightmap=land_mask)
+plot_heightmap_3d(heightmap=heightmap)
--- src/worldgen/terrain/heightmap.py
+++ src/worldgen/terrain/heightmap.py
@@ -1,6 +1,34 @@
 import numpy as np
+import taichi as ti
 
 from .noise import generate_fractal_noise
+
+
+def generate_land_mask(heightmap: np.ndarray, sea_level: float) -> np.ndarray:
+    return heightmap > sea_level
+
+
+@ti.kernel
+def generate_continental_mask_kernel(
+    width: ti.int32, height: ti.int32, output: ti.types.ndarray(dtype=ti.f32, ndim=2)
+):
+    for y, x in output:
+        x_norm = 2.0 * x / (width - 1) - 1.0
+        y_norm = 2.0 * y / (height - 1) - 1.0
+
+        distance = ti.sqrt(x_norm * x_norm + y_norm * y_norm)
+
+        continental_mask = ti.math.clamp(1.0 - distance, 0.0, 1.0)
+
+        output[y, x] = continental_mask
+
+
+def generate_continental_mask(width: int, height: int) -> np.ndarray:
+    output = np.zeros(shape=(height, width), dtype=np.float32)
+
+    generate_continental_mask_kernel(width=width, height=height, output=output)
+
+    return output
 
 
 def generate_heightmap(
